38bf17a3a1942dc0f8745ce67301f9ea6e12456d
Wood <gaolin@hpu.edu.cn>
2018-12-24 10:31:22 +0800

    start with real_apm v1.0.0.0
    initial

---
+++ source_code/WindowsForms_showAPM/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WindowsForms_showAPM
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.notifyIconTaskbar = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIconTaskbar
+            // 
+            this.notifyIconTaskbar.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconTaskbar.Icon")));
+            this.notifyIconTaskbar.Text = "notifyIconText";
+            this.notifyIconTaskbar.Visible = true;
+            this.notifyIconTaskbar.DoubleClick += new System.EventHandler(this.notifyIconTaskbar_DoubleClick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 214);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIconTaskbar;
+    }
+}
+
---
+++ source_code/WindowsForms_showAPM/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_showAPM
+{
+    public partial class FormMain : Form
+    {
+        private int totalAPM = 0;
+        private int thisSecondsCounter = 0;
+        private int oneCounter =0;
+        private int tenCounter = 0;
+        private KeyboardHook k_hook;
+        private MouseHook m_hook;
+        private int[] apmSeconds;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void notifyIconTaskbar_DoubleClick(object sender,EventArgs e)
+        {
+            this.Visible = true;
+            if ( this.WindowState == FormWindowState.Minimized )
+            {
+                this.WindowState = FormWindowState.Normal;
+            } else
+                {
+                    this.WindowState = FormWindowState.Minimized;
+                }
+
+            
+        }
+
+
+        //private void dx_SizeChanged(object sender,EventArgs e)
+        //{
+        //    if ( this.WindowState == FormWindowState.Minimized )
+        //    {
+        //        this.Hide();
+        //    }
+        //}
+        
+
+        private void FormMain_Load(object sender,EventArgs e)
+        {
+            //////////////////
+            ////initial
+            //////////////////
+            
+            ////////////
+            //form
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+
+            ///////////
+            //key hook
+            k_hook = new KeyboardHook();
+            k_hook.KeyDownEvent += new System.Windows.Forms.KeyEventHandler(hook_KeyDown);//钩住键按下 
+            //k_hook.KeyPressEvent += K_hook_KeyPressEvent;
+            k_hook.Start();//安装键盘钩子
+
+            //////////////
+            //mouse hook
+            m_hook = new MouseHook();
+            m_hook.SetHook();
+            //m_hook.MouseMoveEvent += mh_MouseMoveEvent;
+            m_hook.MouseClickEvent += mh_MouseClickEvent;
+
+            /////////////
+            //seconds work array
+            apmSeconds = new int[60];
+            for(int i=0;i<60;i++ )
+            {
+                apmSeconds[i] = 0;
+            }
+
+            /////////////
+            //reset seconds counter
+            thisSecondsCounter = 0;
+
+            /////////////////
+            //install timer
+            System.Timers.Timer t = new System.Timers.Timer(1000);//实例化Timer类，设置间隔时间为10000毫秒；
+            t.Elapsed += new System.Timers.ElapsedEventHandler(checkTheSecondAction);//到达时间的时候执行事件；
+            t.AutoReset = true;//设置是执行一次（false）还是一直执行(true)；
+            t.Enabled = true;//是否执行System.Timers.Timer.Elapsed事件；
+
+        }
+
+
+        public void checkTheSecondAction(object source,System.Timers.ElapsedEventArgs e)
+        {// Core !!!
+            //MessageBox.Show("OK!");
+            calculateSecondAPM();
+
+            //addCounter_one();
+
+            int oneBit = totalAPM % 10;
+            int handredTenBit = ( totalAPM - oneBit ) / 10;
+            string shownAPM = handredTenBit.ToString();
+            SetTaskIconDynamic(shownAPM);
+            System.Console.WriteLine("totalAPM: "+ totalAPM);     //del
+
+            //SetTaskIconDynamic(Convert.ToString(this.tenCounter));      //only test
+
+        }
+
+
+        public void SetTaskIconDynamic(string number)
+        {
+            System.Console.WriteLine("SetTaskIconDynamic");     //del
+            //动态绘制图标样式
+            Size size = this.Icon.Size;
+            Bitmap cursorBitmap = new Bitmap(size.Width,size.Height);
+            Graphics graphics = Graphics.FromImage(cursorBitmap);
+            graphics.Clear(Color.FromArgb(0,0,0,0));
+            graphics.ResetClip();
+            Rectangle rect = new Rectangle(0,0,size.Width,size.Height);
+
+            //Gdi+自定义绘制图标
+            //graphics.DrawImage(this.Icon.ToBitmap(),rect);
+            //graphics.FillEllipse(new SolidBrush(Color.FromArgb(244,107,10)),new Rectangle(rect.Width / 2 - 2,rect.Height / 2 - 2,rect.Width / 2,rect.Height / 2));
+            //graphics.DrawString(number,this.Font,Brushes.White,new Rectangle(rect.Width / 2 - 2,rect.Height / 2 - 2,rect.Width / 2,rect.Height / 2),new StringFormat()
+            //{
+            //    LineAlignment = StringAlignment.Center,
+            //    Alignment = StringAlignment.Center
+            //});
+
+            Font myFont = new Font("sans serif", rect.Width /2  ,FontStyle.Regular);
+            Brush bush = new SolidBrush(Color.Cyan);//填充的颜色
+            //graphics.DrawString("99",myFont,bush,100,100);
+
+            graphics.DrawString(number,myFont,bush,new Rectangle(2,2,rect.Width ,rect.Height),new StringFormat()
+            {
+                LineAlignment = StringAlignment.Center,
+                Alignment = StringAlignment.Center
+            });
+            System.Console.WriteLine("rect.Width :"+ rect.Width);     //del
+            System.Console.WriteLine("rect.Height :"+ rect.Height);     //del
+
+            //生成Icon
+            Icon cursor = Icon.FromHandle(cursorBitmap.GetHicon());
+            graphics.Dispose();
+            cursorBitmap.Dispose();
+
+            //更新任务栏图标样式
+            this.notifyIconTaskbar.Icon = cursor;
+
+            this.notifyIconTaskbar.Text = "Real APM : "+totalAPM;
+
+        }
+
+
+        private void hook_KeyDown(object sender,KeyEventArgs e)
+        {
+            //addCounter_one();     //only for test
+            addOneAPMToSecondsCounter();
+        }
+
+
+        private void mh_MouseClickEvent(object sender,MouseEventArgs e)
+        {
+            //there is a problem,in this method it can listen both click and move
+            // so ,how can we let those action in different method ?? need to finish
+
+
+            //MessageBox.Show(e.X + "-" + e.Y);
+            //System.Console.WriteLine(e.X + "-" + e.Y);     //del
+
+            if( (e.Button == MouseButtons.Left ) ||
+                ( e.Button == MouseButtons.Right )
+              )
+            {
+                //addCounter_one();     //only for test
+                addOneAPMToSecondsCounter();
+            }
+        }
+
+
+        //private void mh_MouseMoveEvent(object sender,MouseEventArgs e)
+        //{
+        //    int x = e.Location.X;
+        //    int y = e.Location.Y;
+        //    textBox1.Text = x + "";
+        //    textBox2.Text = y + "";
+        //}
+
+
+        //private void addCounter_one()
+        //{//this function for test only
+        //    oneCounter++;
+        //    if ( oneCounter >= 10 )
+        //    {
+        //        if(tenCounter < 99)
+        //        {//full counter is below 999
+        //         //full counter: tenCounter * 10 + oneCounter
+        //            tenCounter++;
+        //        }else
+        //            {
+        //                //do-nothing
+        //            }
+        //        oneCounter = 0;
+        //    }
+        //}
+
+
+        private void addOneAPMToSecondsCounter()
+        {
+            thisSecondsCounter++;
+        }
+
+
+        private void calculateSecondAPM()
+        {
+            int secondNow =DateTime.Now.Second;
+            System.Console.WriteLine("secondNow: "+ secondNow);     //del
+            //DateTime.Now.Second.ToString();       //获取秒数 
+
+            apmSeconds[secondNow] = thisSecondsCounter;
+
+            totalAPM = 0;
+            for ( int i = 0; i < 60; i++ )
+            {
+                totalAPM += apmSeconds[i];
+            }
+
+            thisSecondsCounter = 0;     //reset seconds counter
+        }
+
+
+        private void FormMain_FormClosed(object sender,FormClosedEventArgs e)
+        {
+            k_hook.Stop();
+            m_hook.UnHook();
+        }
+    }
+}
